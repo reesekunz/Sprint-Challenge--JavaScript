e94acaf326fa1071ca0d37decba6635ea5f64181
Reese Kunz <reesekunz21@gmail.com>
2019-06-14 09:30:25 -0700

    functions WIP

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, cb)
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
